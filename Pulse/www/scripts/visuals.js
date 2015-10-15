3b89ce2815474d28d625d3fe34651ad1ed8085d3
pietro93 <xpietro93@hotmail.com>
2015-10-15 16:44:12 +0100

    Setup initial animations files and 'neutral' design.

---
+++ Pulse/www/scripts/visuals.js
@@ -0,0 +1,31 @@
+﻿var amoeba = document.createElement('amoeba');
+var container = document.getElementById('container');
+
+function addShape(type) {
+    var time;
+    if (type == 'neutral'){
+        time = Math.floor((Math.random() * 30) + 15);
+    }
+    else if (type == 'calm') {
+        time = Math.floor((Math.random() * 45) + 35);
+    }
+    else if (type == 'excited') {
+        time = Math.floor((Math.random() * 45) + 10);
+    }
+    else {
+        return;
+    }
+    s = document.createElement('shape');
+    s.style.cssText = ("animation: pulserotate "+ time +"s alternate infinite;")
+    amoeba.appendChild(s);
+    }
+
+function addNeutral() {
+    for (var i = 0; i <= 50; i++) {
+        addShape('excited');
+        
+    }
+    container.appendChild(amoeba);
+}
+
+addNeutral();
