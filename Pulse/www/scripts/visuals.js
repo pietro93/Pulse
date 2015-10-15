5f012b615fc88ab579074397fe8fa6a7563bee19
pietro93 <xpietro93@hotmail.com>
2015-10-15 23:57:04 +0100

    Implemented animation for each style (CSS files).

--- Pulse/www/scripts/visuals.js
+++ Pulse/www/scripts/visuals.js
@@ -1,31 +1,42 @@
 ﻿var amoeba = document.createElement('amoeba');
 var container = document.getElementById('container');
 
+function setStyle(style) {
+    document.head.innerHTML.anchor('<link href="css/'+ style +'.css" rel="stylesheet" />');
+}
+
 function addShape(type) {
     var time;
+    var border_radius;
     if (type == 'neutral'){
         time = Math.floor((Math.random() * 30) + 15);
+        border_radius = 15;
     }
     else if (type == 'calm') {
-        time = Math.floor((Math.random() * 45) + 35);
+        time = Math.floor((Math.random() * 45) + 40);
+        border_radius = 50;
     }
     else if (type == 'excited') {
-        time = Math.floor((Math.random() * 45) + 10);
+        time = Math.floor((Math.random() * 30) + 10);
+        border_radius = 0;
     }
     else {
         return;
     }
     s = document.createElement('shape');
-    s.style.cssText = ("animation: pulserotate "+ time +"s alternate infinite;")
+    s.style.cssText = ("border-radius: " + border_radius + "px; " +
+        "animation: pulserotate "+ time +"s linear alternate infinite;")
     amoeba.appendChild(s);
     }
 
 function addNeutral() {
-    for (var i = 0; i <= 50; i++) {
-        addShape('excited');
+    for (var i = 0; i <= 10;i++) {
+        addShape('calm');
         
     }
     container.appendChild(amoeba);
+    amoeba.style.cssText = ("animation: pulse " + 10+ "s alternate infinite;")
 }
 
+setStyle('calm');
 addNeutral();
