8f057ed6fce81f155eaede9097f74f420f7bbb08
pietro93 <xpietro93@hotmail.com>
2015-10-18 22:51:33 +0100

    Cleaned everything. Made colours of neutral and excited amoebas a bit darker.

--- Pulse/www/scripts/amoeba.js
+++ Pulse/www/scripts/amoeba.js
@@ -1,15 +1,14 @@
 ﻿var amoeba = document.getElementById('amoeba');
 var container = document.getElementById('container');
+
 var status = "neutral";
 
-function setStyle(style) {
-    document.getElementById('stylesheet').setAttribute('href', 'css/'+style+'.css');
-}
-
+// Get random number
 function rand(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
 
+// Add 'shape' that will make up the amoeba.
 function addShape(type) {
     var time;
     var border_radius;
@@ -21,10 +20,12 @@ function addShape(type) {
         time = rand(20,30);
         animation = "pulserotate";
     }
+
     else if (type == 'calm') {
         time = rand(30,40);
         animation = "shrink";
     }
+
     else if (type == 'excited') {
         time = rand(10, 20);
         opacity = "0";
@@ -37,7 +38,7 @@ function addShape(type) {
     amoeba.appendChild(s);
 }
 
-function addNeutral() {
+function createAmoeba() {
     for (var i = 0; i < 50; i++) {
         addShape('excited');
     }
@@ -52,12 +53,13 @@ function addNeutral() {
 
 function excite() {
     $("#amoeba  :nth-child(even)").css({ "background": "red" });
-    $("#amoeba :nth-child(odd)").css({ "background": "orange" });
+    $("#amoeba :nth-child(odd)").css({ "background": "orangered" });
     $("#amoeba > *").animate({
         "opacity":"0.2",
         "border-radius": "1px",
         height: "70%", width: "70%"}, 1000);
     status = "excited";
+    $("#amoeba").css({ "animation": "pulsate " + 10 + "s linear alternate infinite" });
 }
 
 function cooldown() {
@@ -66,48 +68,21 @@ function cooldown() {
     $("#amoeba :nth-child(-n+90)").css({ "opacity": "0"});
     $("#amoeba > *").animate({
         "border-radius": "50px",
-        height: "55%", width: "55%"}, 1000);
+        height: "55%", width: "55%"
+    }, 1000);
     status = "calm";
+    amoeba.style.cssText = ("animation: pulsate " + 25 + "s linear alternate infinite;");
 }
 
 function normalise() {
-    $("#amoeba :nth-child(even)").css({ "background": "purple" });
-    $("#amoeba :nth-child(odd)").css({ "background": "grey" });
+    $("#amoeba :nth-child(even)").css({ "background": "grey" });
+    $("#amoeba :nth-child(odd)").css({ "background": "mediumpurple" });
     $("#amoeba :nth-child(-n+80)").css({ "opacity": "0.2" });
     $("#amoeba :nth-child(-n+50)").css({ "opacity": "0" });
     $("#amoeba > *").animate({
-        "border-radius": "25px",
+        "border-radius": "20px",
         height: "60%", width: "60%"
     }, 1000);
     status = "neutral";
+    amoeba.style.cssText = ("animation: pulsate " + 15 + "s linear alternate infinite;");
 }
-
-function addCalm() {
-    for (var i = 0; i <= 15; i++) {
-        addShape('calm');
-    }
-    container.appendChild(amoeba);
-    amoeba.style.cssText = ("animation: pulse " + 15 + "s ease-in-out alternate infinite;")
-}
-
-function addExcited() {
-    for (var i = 0; i <= 50; i++) {
-        addShape('excited');
-    }
-    amoeba.style.cssText = ("animation: pulse " + 5 + "s  linear alternate infinite;")
-}
-
-
-//$("container").click(function () {
-//    if (status == "neutral")
-//        cooldown();
-//    if (status == "calm") {
-//        normalise();
-//        excite();
-//    }
-//    if (status == "excited") {
-//        normalise();
-//        cooldown();
-//    }
-//})
-
--- Pulse/www/scripts/transition.js
+++
@@ -1,5 +0,0 @@
-﻿function setStyle(style) {
-    document.getElementById('stylesheet').setAttribute('href', 'css/' + style + '.css');
-}
-
-setStyle(calm);
